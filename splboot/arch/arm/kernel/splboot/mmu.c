32f6a400c9bb478794fbbe044add586225511ea2
Ankur Kaushik <kaushik.ankur204@gmail.com>
2013-01-31 00:10:00 +0530

    minor Update 1

--- splboot/arch/arm/kernel/splboot/mmu.c
+++ splboot/arch/arm/kernel/splboot/mmu.c
@@ -2,8 +2,7 @@
 /*
  *  linux/arch/arm/mm/mmu.c
  *
- *  Copyright (C) 1995-2005 Russell King
- *
+ * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
